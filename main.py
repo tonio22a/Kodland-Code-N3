d0030c3ddc2269d45aa06751dc681e05fce14e66
tonio22a <115809114+tonio22a@users.noreply.github.com>
2024-10-27 14:05:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-meme_dict = {
-    "КРИНЖ": "Что-то очень странное или стыдное",
-    "ЛОЛ": "Что-то очень смешное",
-    "РОФЛ": "Шутка",
-    "КРИПОВЫЙ": "Страшный"
-}
+#!/usr/bin/python
 
-print("Привет!")
+# This is a simple bot with schedule timer
+# https://schedule.readthedocs.io
 
-for i in range(5):
-    word = input("Введите непонятное слово (большими буквами!): ")
+import time, threading, schedule
+import telebot
 
-    if word in meme_dict:
-        print("Слово найдено! Значение:", meme_dict[word])
+API_TOKEN = '7876584346:AAHId83U9P5u0faYAsZEmjOu_5wnWldh74c'
+bot = telebot.TeleBot(API_TOKEN)
+
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! используй /set (sec) для того, чтобы поставить таймер")
+
+
+def beep(chat_id) -> None:
+    """Send the beep message."""
+    bot.send_message(chat_id, text='Туту!')
+
+
+@bot.message_handler(commands=['set'])
+def set_timer(message):
+    args = message.text.split()
+    if len(args) > 1 and args[1].isdigit():
+        sec = int(args[1])
+        schedule.every(sec).seconds.do(beep, message.chat.id).tag(message.chat.id)
     else:
-        print("Слово не найдено!")
+        bot.reply_to(message, 'Используй: /set <sec>')
+
+
+@bot.message_handler(commands=['ping'])
+def poping(message):
+    bot.reply_to(message, 'Pong!')
+
+@bot.message_handler(commands=['unset'])
+def unset_timer(message):
+    schedule.clear(message.chat.id)
+
+
+if __name__ == '__main__':
+    threading.Thread(target=bot.infinity_polling, name='bot_infinity_polling', daemon=True).start()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
