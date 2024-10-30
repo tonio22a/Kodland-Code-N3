99646fa54a7155180c7b8734bd6609dca7e08146
tonio22a <115809114+tonio22a@users.noreply.github.com>
2024-10-30 11:59:00 +0200

    Обновление бота - main.py
    Добавлена команда /ping - показывает пинг
    Улучшен перевод на русский язык
    Бот отвечает на не существующие в программе команды

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-#!/usr/bin/python
-
-# This is a simple bot with schedule timer
-# https://schedule.readthedocs.io
-
-import time, threading, schedule
+import time
+import threading
+import schedule
 import telebot
 
 API_TOKEN = '7876584346:AAHId83U9P5u0faYAsZEmjOu_5wnWldh74c'
 bot = telebot.TeleBot(API_TOKEN)
 
 
-@bot.message_handler(commands=['help', 'start'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! используй /set (sec) для того, чтобы поставить таймер")
+    bot.reply_to(message, "Привет! используйте /help для того, чтобы прочитать список команд")
 
 
 def beep(chat_id) -> None:
     """Send the beep message."""
-    bot.send_message(chat_id, text='Туту!')
+    bot.send_message(chat_id, text='Таймер!')
 
 
 @bot.message_handler(commands=['set'])
@@ -32,11 +29,34 @@ def set_timer(message):
 
 @bot.message_handler(commands=['ping'])
 def poping(message):
-    bot.reply_to(message, 'Pong!')
+    start_time = time.monotonic()
+    reply_message = bot.reply_to(message, 'Мы анализируем ваш ping.')
+
+    ping = (time.monotonic() - start_time) * 1000
+    bot.edit_message_text(chat_id=message.chat.id, message_id=reply_message.message_id, text=f'Ваш пинг: {int(ping)}ms. Pong!')
+
 
 @bot.message_handler(commands=['unset'])
 def unset_timer(message):
     schedule.clear(message.chat.id)
+    bot.reply_to(message, "Таймер успешно остановлен и выключен.")
+
+
+@bot.message_handler(commands=["help"])
+def helpus(message):
+    bot.reply_to(
+        message,
+        "/set - ставит таймер\n"
+        "/start - выдаёт приветствие\n"
+        "/unset - удаляет таймер\n"
+        "/ping - проверяет текущий пинг\n"
+        "/help - выдаёт данное сообщение"
+    )
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, "Кажется, такой команды нет ☹️\n"
+                 "Используйте команду /help, чтобы прочитать список доступных команд.")
 
 
 if __name__ == '__main__':
